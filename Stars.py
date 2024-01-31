6df0b374eb2352a24f5bef2033522e551c8cee4c
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-01-31 21:36:32 +0530

    stars

---
+++ Stars.py
@@ -0,0 +1,22 @@
+# n = 4
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*",end= " ")
+#     print()
+
+# n = 4
+# for i in range(n):
+#     for j in range(i,n):
+#         print("*",end= " ")
+#     print()
+
+#star pattern 
+#right triangle 
+n = 6 
+for i in  range(n):
+    for j in range(i,n):
+        print(" ",end= " ")
+
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
