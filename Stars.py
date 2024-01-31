5f677dcd2115cd0782c44c8d2a4fcea61112c717
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-01-31 21:29:56 +0530

    stars

--- Stars.py
+++ Stars.py
@@ -4,8 +4,19 @@
 #         print("*",end= " ")
 #     print()
 
-n = 4
-for i in range(n):
+# n = 4
+# for i in range(n):
+#     for j in range(i,n):
+#         print("*",end= " ")
+#     print()
+
+#star pattern 
+
+n = 6 
+for i in  range(n):
     for j in range(i,n):
-        print("*",end= " ")
+        print(" ",end= " ")
+
+    for j in range(i+1):
+        print("*",end=" ")
     print()
