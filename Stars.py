7b1061667c8d791d305e3dedb1d37bd4b49f6f2e
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-02-01 14:35:45 +0530

    stars

--- Stars.py
+++ Stars.py
@@ -42,15 +42,16 @@
 #         print("*",end=" ")
 #     print()
 
+# Reverse hill star pattern
 n = 5
 for i in  range(n):
     for j in range(i+1):
         print(" ",end= " ")
 
-    for j in range(i,n):
+    for j in range(i,n-1):
         print("*",end=" ")
 
-    for j in range(i*n):
+    for j in range(i,n):
         print("*",end=" ")
     print()
 
