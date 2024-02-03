47b94420ccfbd252bca2dc7bc6fb1bbfb00796a1
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-02-03 20:07:43 +0530

    built in functions

--- Builtin_Functions.py
+++ Builtin_Functions.py
@@ -34,3 +34,8 @@
 numbers = [1, 2, 3, 4, 5]
 squared = list(map(lambda x: x**2, numbers))
 print("Squared numbers:", squared)
+
+#filter function
+X = [1,2,3,4,5,6,7]
+even_numbers = list(filter(lambda x: x %2 == 0, X))
+print("even numbers =",even_numbers)
