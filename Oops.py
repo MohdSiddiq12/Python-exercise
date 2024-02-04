c875871e7f40f49204c4ae0d79a6f1773c893911
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-02-04 20:48:56 +0530

    oops

--- Oops.py
+++ Oops.py
@@ -9,3 +9,19 @@ class Dog:
 my_dog = Dog("Buddy", 3)
 print(my_dog.name)
 my_dog.bark()
+
+#explanation 
+#Dog is the name of the class.
+#__init__ is a special method (constructor) that gets called when an object is created. It initializes the attributes of the object.
+#self is a reference to the instance of the class (the object itself).
+#name and age are attributes of the class.
+
+#Objects:
+#An object is an instance of a class. It is a concrete entity created from the class blueprint. 
+#Objects have attributes (characteristics) and methods (functions) associated with them.
+class method:
+    def __init__(self):
+        print("hello from method")
+    
+x = method()
+print(x)
