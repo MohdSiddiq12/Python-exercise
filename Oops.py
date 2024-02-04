d386d3767bf0977502b6e04590193578b034751b
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-02-04 20:39:07 +0530

    oops

--- Oops.py
+++ Oops.py
@@ -1,12 +1,10 @@
 # Class and Object
 class Dog:
-    def __init__(self, name, age):
+    def __init__(self,name,age):
         self.name = name
         self.age = age
-
     def bark(self):
-        print(f"{self.name} says Woof!")
-
+        print(f"{self.name},says woof!")
 # Creating an object
 my_dog = Dog("Buddy", 3)
 print(my_dog.name)
