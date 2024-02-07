3a73209b83714e4bd80abca1745792ad54a7220b
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-02-07 17:59:46 +0530

    multi level inheritance

--- inheritance.py
+++ inheritance.py
@@ -17,3 +17,30 @@ my_dog = Dog("khaled")
 
 print(my_dog.species)
 print(my_Animal.species)
+
+#multiple inheritance
+#Multiple inheritance allows a class to inherit from more than one parent class.
+
+class engine:
+    def start_engine(self):
+        print("engine started")
+    def stop_engine(self):
+        print("engine stopped")
+class body:
+    def open_door(self):
+        print("door opened")
+    def close_door(self):
+        print("door closed")
+class car(engine,body):
+    def drive(self):
+        print("car is in motion")
+    def park(self):
+        print("car is parked")
+
+my_car = car()
+my_car.start_engine()
+my_car.stop_engine()
+my_car.open_door()
+my_car.close_door()
+my_car.drive()
+my_car.park()
