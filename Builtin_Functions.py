27ebe4ad996005f8f8147f64a751a1438d1da99f
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-02-12 21:35:32 +0530

    Update Builtin_Functions.py

--- Builtin_Functions.py
+++ Builtin_Functions.py
@@ -37,7 +37,7 @@ print("Squared numbers:", squared)
 
 #filter function
 X = [1,2,3,4,5,6,7]
-even_numbers = list(filter(lambda x: x %2 == 0, X))
+even_numbers = list(filter(lambda x: x %2 == 0, X))   
 print("even numbers =",even_numbers)
 
 zipped = list(zip(numbers,X))
