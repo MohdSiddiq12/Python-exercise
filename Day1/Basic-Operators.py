d456025219e163fd15d792a8c386968631cc3618
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-16 07:20:32 +0530

    day one question 4

--- Day1/Basic-Operators.py
+++ Day1/Basic-Operators.py
@@ -7,19 +7,36 @@
 #Write a Python program that prompts the user to enter two numbers and then performs addition, subtraction,
 # multiplication, and division on those numbers, printing the results.
 
-x = float(input("enter x "))
-y = float(input("enter y "))
+# x = float(input("enter x "))
+# y = float(input("enter y "))
 
-addition = x + y
-subtraction = x - y
-multiplication = x * y
-if y != 0:
-    division = x / y
-else:
-    print("cannot divide by zero")
+# addition = x + y
+# subtraction = x - y
+# multiplication = x * y
+# if y != 0:
+#     division = x / y
+# else:
+#     print("cannot divide by zero")
 
 
-print("addition", x, "+",  y, "=", addition)
-print("subtraction", x, "-", y, "=", subtraction)
-print("multiplication",  x, "*",  y, "=", multiplication)
-print("division",  x, "/",  y, "=", division)
+# print("addition", x, "+",  y, "=", addition)
+# print("subtraction", x, "-", y, "=", subtraction)
+# print("multiplication",  x, "*",  y, "=", multiplication)
+# print("division",  x, "/",  y, "=", division)
+
+#Input/Output:
+#Write a Python program that asks the user for their age and then prints out the age.
+
+# age = int(input("enter your age: "))
+# print("Your age is ",age)
+
+#Conditional Statements:
+# Write a Python program that takes a number as input and prints whether it is positive, negative, or zero.
+
+# number = int(input("enter any number: "))
+# if number >= 1:
+#     print("number is positive")
+# elif number <= -1:
+#     print("number is negative")
+# else:
+#     print("number is zero")
