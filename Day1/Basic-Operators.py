9ed384bd3502d0c60d7563e8f641eee44272ffbb
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-16 06:12:48 +0530

    day one

---
+++ Day1/Basic-Operators.py
@@ -0,0 +1,5 @@
+#Variable and Data Types:
+#Write a Python program that takes the user's name as input and greets them with "Hello, [name]!"
+
+name = input("Enter your name ")
+print("Hello," + name + " Good Morning")
