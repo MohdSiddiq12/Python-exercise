bb84bebc0a1b1dd9396f4c4db20ac15494b7fae7
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-16 06:24:34 +0530

    day one question 2

--- Day1/Basic-Operators.py
+++ Day1/Basic-Operators.py
@@ -1,5 +1,25 @@
 #Variable and Data Types:
 #Write a Python program that takes the user's name as input and greets them with "Hello, [name]!"
 
-name = input("Enter your name ")
-print("Hello," + name + " Good Morning")
+# 
+
+#Basic Operators:
+#Write a Python program that prompts the user to enter two numbers and then performs addition, subtraction,
+# multiplication, and division on those numbers, printing the results.
+
+x = float(input("enter x "))
+y = float(input("enter y "))
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+if y != 0:
+    division = x / y
+else:
+    print("cannot divide by zero")
+
+
+print("addition", x, "+",  y, "=", addition)
+print("subtraction", x, "-", y, "=", subtraction)
+print("multiplication",  x, "*",  y, "=", multiplication)
+print("division",  x, "/",  y, "=", division)
