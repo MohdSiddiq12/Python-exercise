8d9ea26807a56d9b8e6d401e3eeb8cec85cf0b15
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-18 09:51:46 +0530

    question2

--- Day2/Data-Structures.py
+++ Day2/Data-Structures.py
@@ -8,14 +8,24 @@
 # Write a Python program to create a list of your favorite movies and print each movie.
 
 movies = ['slumdog millionare','inception','dark knight rises','Batman','The pursuit of happiness']
-for i in movies:
-    print(i)
+# for i in movies:
+#     print(i)
 
 # List Operations:
 # Write a Python program to perform the following operations on a list:
 
 # Append a new item.
+
+
+movies.append('interstellar')
+
+
 # Remove an item.
+
+movies.remove('slumdog millionare')
+
+
 # Sort the list.
 # Reverse the list.
 # Print the length of the list.
+    
