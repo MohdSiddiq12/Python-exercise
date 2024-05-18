69fbed501caff84a969bb7c37adc1b11d88566aa
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-18 09:42:44 +0530

    day 2

---
+++ Day2/Data-Structures.py
@@ -0,0 +1,21 @@
+# Topics to Cover
+# Lists
+# Tuples
+# Dictionaries
+# Sets
+
+# Create a List:
+# Write a Python program to create a list of your favorite movies and print each movie.
+
+movies = ['slumdog millionare','inception','dark knight rises','Batman','The pursuit of happiness']
+for i in movies:
+    print(i)
+
+# List Operations:
+# Write a Python program to perform the following operations on a list:
+
+# Append a new item.
+# Remove an item.
+# Sort the list.
+# Reverse the list.
+# Print the length of the list.
