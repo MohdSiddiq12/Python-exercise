b367114cdff38f6e10f756eb6c861bb52f20bf2b
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-20 09:01:43 +0530

    day 3 control flow

--- Day3/Control-flow.py
+++ Day3/Control-flow.py
@@ -9,11 +9,26 @@
 # Simple if Statement:
 # Write a Python program that checks if a number is positive, negative, or zero and prints an appropriate message.
 
-number = int(input("enter a number"))
-if number >= 1:
-    print('number is positive')
-elif number <= -1:
-    print("number is negative")
-else:
-    print("number is zero")
+# number = int(input("enter a number"))
+# if number >= 1:
+#     print('number is positive')
+# elif number <= -1:
+#     print("number is negative")
+# else:
+#     print("number is zero")
 
+# if-elif-else Statement:
+# Write a Python program that asks the user for their age and prints whether they are a child, teenager, adult, or senior.
+
+age = int(input("enter your age: "))
+if age <= 5:
+    print("You are child")
+elif age <= 18:
+    print("you are teenager")
+elif age <= 40:
+    print("you are an adult")
+else:
+    print("you are senior")
+
+# Nested if Statements:
+# Write a Python program that checks if a number is even or odd and if it is divisible by 3.
