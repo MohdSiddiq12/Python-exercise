83859a658d28a9981d42e417578e7a34a99c2fb1
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-20 08:57:42 +0530

    day 3 control flow

--- Day3/Control-flow.py
+++ Day3/Control-flow.py
@@ -6,3 +6,14 @@
 # Learn how to use for and while loops to iterate over sequences and execute code repeatedly.
 # Practice using break and continue statements to control loop execution
 
+# Simple if Statement:
+# Write a Python program that checks if a number is positive, negative, or zero and prints an appropriate message.
+
+number = int(input("enter a number"))
+if number >= 1:
+    print('number is positive')
+elif number <= -1:
+    print("number is negative")
+else:
+    print("number is zero")
+
