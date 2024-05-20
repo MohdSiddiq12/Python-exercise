a07428d4278ace83911992bfae1f42770af974c6
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-20 08:54:45 +0530

    day 3 control flow

---
+++ Day3/Control-flow.py
@@ -0,0 +1,8 @@
+# Topics to Cover
+# Conditional Statements
+# Loops
+# Learning Objectives
+# Understand how to use if, elif, and else statements.
+# Learn how to use for and while loops to iterate over sequences and execute code repeatedly.
+# Practice using break and continue statements to control loop execution
+
