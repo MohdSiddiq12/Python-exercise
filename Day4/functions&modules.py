d79a9de4e34efff8b48e222f88f4bb030b757584
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-21 11:17:43 +0530

    day 4 functions and modules

---
+++ Day4/functions&modules.py
@@ -0,0 +1,32 @@
+# Topics to Cover
+# Functions
+# Function Parameters
+# Return Statement
+# Modules
+# Importing Modules
+# Learning Objectives
+# Understand how to define and call functions.
+# Learn about function parameters and their types (positional, keyword, default, variable-length).
+# Understand the purpose and usage of the return statement.
+# Learn how to create and use modules to organize code.
+# Exercises
+# Functions
+# Simple Function:
+# Write a Python function that takes two numbers as input and returns their sum.
+def add(x,y):
+    return x + y
+num = add(1,2)
+print(num)
+
+# Function with Parameters:
+# Write a Python function that takes a list of numbers as input and returns the maximum value from the list.
+
+# Return Statement
+# Function with Return Statement:
+# Write a Python function that checks if a number is prime and returns True if it is, False otherwise.
+# Modules
+# Create a Module:
+# Create a Python module named calculator.py that contains functions for addition, subtraction, multiplication, and division.
+
+# Import Module:
+# Write a Python program that imports the calculator.py module and uses its functions to perform arithmetic operations.
