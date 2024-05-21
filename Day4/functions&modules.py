40d2064f0305617df46d9d8e025343be15b25b56
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-05-21 11:43:14 +0530

    g

--- Day4/functions&modules.py
+++ Day4/functions&modules.py
@@ -20,14 +20,30 @@
 
 # Function with Parameters:
 # Write a Python function that takes a list of numbers as input and returns the maximum value from the list.
-def maximum(*args):
-    return max(*args)
-num = maximum(1,2,3,4,5,6,11,22,33,44)
-print(num)
+# def maximum(*args):
+#     return max(*args)
+# num = maximum(1,2,3,4,5,6,11,22,33,44)
+# print(num)
 
 # Return Statement
 # Function with Return Statement:
 # Write a Python function that checks if a number is prime and returns True if it is, False otherwise.
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num == 2:
+        return True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+
+    return True
+number = 11
+if is_prime(number):
+    print(f"{number} is prime.")
+else:
+    print(f"{number} is not prime.")
 # Modules
 # Create a Module:
 # Create a Python module named calculator.py that contains functions for addition, subtraction, multiplication, and division.
