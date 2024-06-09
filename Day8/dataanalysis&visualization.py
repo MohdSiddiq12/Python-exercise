41f225ba1c24e2ed6b5457d1c5329a0540b58f7d
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-06-09 18:56:12 +0530

    day 8

--- Day8/dataanalysis&visualization.py
+++ Day8/dataanalysis&visualization.py
@@ -11,13 +11,25 @@
 # Learn how to use Pandas for data manipulation and analysis.
 # Understand how to create basic plots using Matplotlib.
 
+# Create a NumPy Array:
+# Write a Python program to create a NumPy array of 10 zeros, 10 ones, and 10 fives.
 import numpy as np
 
-# Create arrays of zeros, ones, and fives
-zeros = np.zeros(10)
-ones = np.ones(10) * 2
-fives = np.ones(10) * 5
+# # Create arrays of zeros, ones, and fives
+# zeros = np.zeros(10)
+# ones = np.ones(10) * 2
+# fives = np.ones(10) * 5
 
-print("Zeros:", zeros)
-print("Ones:", ones)
-print("Fives:", fives)
+# print("Zeros:", zeros)
+# print("Ones:", ones)
+# print("Fives:", fives)
+
+
+# Array Operations:
+# Write a Python program to create a NumPy array of integers from 1 to 20. Reshape the array to 4x5 and calculate the sum of each column
+arr = np.arange(1,21)
+reshaped_arr = arr.reshape(4, 5)
+
+col = reshaped_arr.sum(axis=0)
+print("reshaped_array: ",reshaped_arr)
+print(col)
