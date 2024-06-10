2bd9fbec154cacdbec68b27b145e79ad01b441c6
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-06-10 16:25:57 +0530

    day 9

---
+++ Day9/dataanalysis&manipulation.py
@@ -0,0 +1,25 @@
+
+# For Day 9, let's focus on advanced data analysis and manipulation using Pandas. 
+# This will include more complex operations such as merging, grouping, pivoting, and handling missing data.
+
+# Topics to Cover
+# Advanced DataFrame operations
+# Merging and joining DataFrames
+# Grouping and aggregation
+# Pivot tables
+# Handling missing data
+# Learning Objectives
+# Understand how to perform advanced DataFrame manipulations.
+# Learn how to merge and join DataFrames.
+# Understand grouping and aggregation for data analysis.
+# Learn how to create and manipulate pivot tables.
+# Understand techniques for handling missing data in DataFrames
+
+# Selecting and Filtering:
+# Write a Python program to select specific columns and filter rows based on a condition in a DataFrame.
+
+data = {
+    'name': ['bob','marley','roca','zendaya'],
+    'age': [17,22,35,23],
+
+}
