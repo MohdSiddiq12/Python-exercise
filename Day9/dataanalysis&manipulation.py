fe68eb8911e9f160b0f2a5fa343e2c3baf9d6497
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-06-11 19:31:06 +0530

    day 9

--- Day9/dataanalysis&manipulation.py
+++ Day9/dataanalysis&manipulation.py
@@ -61,3 +61,29 @@ import pandas as pd
 
 # Join DataFrames:
 # Write a Python program to perform different types of joins (inner, outer, left, right) on two DataFrames.
+import pandas as pd
+
+# Create two DataFrames
+df1 = pd.DataFrame({
+    'ID': [1, 2, 3, 4],
+    'Name': ['Alice', 'Bob', 'Charlie', 'David']
+})
+df2 = pd.DataFrame({
+    'ID': [3, 4, 5, 6],
+    'Age': [22, 32, 29, 24]
+})
+
+# Set 'ID' as the index
+df1.set_index('ID', inplace=True)
+df2.set_index('ID', inplace=True)
+
+# Perform different types of joins
+inner_join = df1.join(df2, how='inner')
+outer_join = df1.join(df2, how='outer')
+left_join = df1.join(df2, how='left')
+right_join = df1.join(df2, how='right')
+
+print("Inner Join:\n", inner_join)
+print("Outer Join:\n", outer_join)
+print("Left Join:\n", left_join)
+print("Right Join:\n", right_join)
