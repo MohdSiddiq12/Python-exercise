bd5ba8985f6061f49f1e4fc02717d603a23bada7
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-07-16 15:00:14 +0530

    updates

--- Day2/Data-Structures.py
+++ Day2/Data-Structures.py
@@ -1,18 +1,18 @@
-# # # Topics to Cover
-# # # Lists
-# # # Tuples
-# # # Dictionaries
-# # # Sets
+# Topics to Cover
+# Lists
+# Tuples
+# Dictionaries
+# Sets
 
-# # # Create a List:
-# # # Write a Python program to create a list of your favorite movies and print each movie.
+# Create a List:
+# Write a Python program to create a list of your favorite movies and print each movie.
 
-# # movies = ['slumdog millionare','inception','dark knight rises','Batman','The pursuit of happiness']
-# # # for i in movies:
-# # #     print(i)
+movies = ['slumdog millionare','inception','dark knight rises','Batman','The pursuit of happiness']
+for i in movies:
+    print(i)
 
-# # # List Operations:
-# # # Write a Python program to perform the following operations on a list:
+# List Operations:
+#Write a Python program to perform the following operations on a list:
 
 movies.append('interstellar') # Append a new item.
 
